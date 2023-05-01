f12e7a5939f840130b7e26ed01593e98d9fb3195
aaron <rau_aaron@protonmail.com>
2023-05-01 21:41:37 +0200

    read html from website and also create a file for the news feed

--- forss/__main__.py
+++ forss/__main__.py
@@ -1,22 +1,28 @@
 import logging
+import sys
 import xml.etree.cElementTree as ET
 
 import bs4
 
 import rss
+import web
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
+    url: str = "https://www.htw-berlin.de/hochschule/aktuelles/"
     feed = rss.new_feed(
         "HTW Termine",
-        "https://www.htw-berlin.de/hochschule/aktuelles/",
+        url,
         "Aktuelles zur HTW",
     )
 
-    with open("Aktuelles.html", "r") as html_page:
-        soup = bs4.BeautifulSoup(html_page, "html.parser")
+    response_code, response_text = web.get_html(url)
+    if response_code != 200:
+        sys.exit(1)
+
+    soup = bs4.BeautifulSoup(response_text, "html.parser")
 
     termine = soup.find(class_="terminkalender")
 
@@ -33,3 +39,25 @@ if __name__ == "__main__":
 
     tree = ET.ElementTree(feed)
     tree.write("termine.rss", encoding="utf-8")
+
+    feed = rss.new_feed(
+        "HTW News",
+        url,
+        "Aktuelles zur HTW",
+    )
+
+    soup = bs4.BeautifulSoup(response_text, "html.parser")
+    news = soup.find(class_="newslist")
+
+    for article in news.find_all("article"):
+        rss.add_article(
+            feed,
+            title=article.find("h1").text,
+            description=article.find("p").text,
+            link=article.find("a").get("href"),
+        )
+
+    ET.indent(feed)
+
+    tree = ET.ElementTree(feed)
+    tree.write("news.rss", encoding="utf-8")
