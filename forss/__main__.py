c4cdfd2bf0f7b21665c5d8f1383e59e643293b9a
aaron <rau_aaron@protonmail.com>
2023-05-01 21:22:59 +0200

    create rss for termine

--- forss/__main__.py
+++ forss/__main__.py
@@ -1,7 +1,35 @@
 import logging
+import xml.etree.cElementTree as ET
+
+import bs4
+
+import rss
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
-    print("hello snake")
+    feed = rss.new_feed(
+        "HTW Termine",
+        "https://www.htw-berlin.de/hochschule/aktuelles/",
+        "Aktuelles zur HTW",
+    )
+
+    with open("Aktuelles.html", "r") as html_page:
+        soup = bs4.BeautifulSoup(html_page, "html.parser")
+
+    termine = soup.find(class_="terminkalender")
+
+    for article in termine.find_all("article"):
+        date = f'[{article.find(class_="month").text}/{article.find(class_="day").text}]'
+        rss.add_article(
+            feed,
+            title=f'{date} {article.find("h1").text}',
+            description=article.find("p").text,
+            link=article.find("a").get("href"),
+        )
+
+    ET.indent(feed)
+
+    tree = ET.ElementTree(feed)
+    tree.write("termine.rss", encoding="utf-8")
